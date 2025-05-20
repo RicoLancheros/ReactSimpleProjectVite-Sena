9947040e282fc5a432afe9e6d04385a1dbc1ad60
Hurtadx <andreshm253@gmail.com>
2025-05-20 14:28:02 -0500

    Clean up imports in main.jsx: remove unnecessary comments for improved readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react' //import react
-import ReactDOM from 'react-dom/client' //import react-dom
-import App from './App.jsx' //import App 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom' //import react-router-dom
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-//import pages
 import HomePage from './pages/HomePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import ContactPage from './pages/ContactPage.jsx';
 import ServicesPage from './pages/ServicesPage.jsx';
 
-// import './Styles/Styles.css'; // Descomentar si se crea un archivo de estilos propio
+import './Styles/Styles.css';
 
-//render
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
